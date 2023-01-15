773505bd14669514b93c54eabf779b8cb5752228
kramaranya <AKramarIK02@office365.fiot.kpi.ua>
2023-01-15 21:24:42 +0100

    Add JWTManager and error handling

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 import os
 
+from flask import jsonify
+
 from recipes import app
 from flask_smorest import Api
 from recipes.db import db
@@ -26,6 +28,35 @@ api = Api(app)
 db.init_app(app)
 
 jwt = JWTManager(app)
+
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+   return (
+       jsonify({"message": "The token has expired.", "error": "token_expired"}),
+       401,
+   )
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+   return (
+       jsonify(
+           {"message": "Signature verification failed.", "error": "invalid_token"}
+       ),
+       401,
+   )
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+   return (
+       jsonify(
+           {
+               "description": "Request does not contain an access token.",
+               "error": "authorization_required",
+           }
+       ),
+       401,
+   )
+
 
 with app.app_context():
     db.create_all()
